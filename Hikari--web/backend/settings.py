d48e727aed10adf80628015e6ea5097194a3d0ec
Juan Diego Ibañez <juandiegoiv16@gmail.com>
2025-12-01 14:23:32 -0500

    conexion de base de datos

--- Hikari--web/backend/settings.py
+++ Hikari--web/backend/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moodmyapp_dbt',
+        'USER': 'root',
+        'PASSWORD': '2202',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
